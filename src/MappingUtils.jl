60b46f8367e68079a62fe110847a3c5428f9d541
singularitti <singularitti@outlook.com>
2022-11-23 04:27:56 -0500

    Add `mapat` & `mapat!`
    See https://discourse.julialang.org/t/define-a-mapat-function-to-map-a-function-over-specific-indices/90392

--- src/MappingUtils.jl
+++ src/MappingUtils.jl
@@ -1,5 +1,20 @@
 module MappingUtils
 
-# Write your package code here.
+export mapat, mapat!
+
+# Map function `f` at specific indices of an array and update in-place
+function mapat!(f, array, indices...)
+    area = view(array, indices...)
+    map!(f, area, area)
+    return array
+end
+
+# Map function `f` at specific indices of an array
+function mapat(f, A, indices...)
+    B = deepcopy(A)
+    area = view(B, indices...)
+    map!(f, area, area)
+    return B
+end
 
 end
