01dbf01c1cb8a1d770e2657896ae21bedc2dd74d
singularitti <singularitti@outlook.com>
2022-12-01 09:15:02 -0500

    Add `mapnested` for multiple levels

--- src/MappingUtils.jl
+++ src/MappingUtils.jl
@@ -18,5 +18,21 @@ function mapat(f, A, indices...)
 end
 
 mapnested(f, A, level) = isone(level) ? map(f, A) : map(B -> mapnested(f, B, level - 1), A)
+function mapnested(f, A, level₁, level₂, levels...)
+    levels = Base.vect(level₁, level₂, levels...)
+    return _mapnested(f, A, 1, sort(levels))
+end
+function _mapnested(f, A, currentlevel, levels)
+    @show levels
+    if isempty(levels)
+        return map(f, A)
+    else
+        @show currentlevel
+        f′ = currentlevel == first(levels) ? f : identity
+        @show f′
+        nextlevel = currentlevel + 1  # Do not do in-place
+        return _mapnested(f′, A, nextlevel, Base.rest(levels, nextlevel))
+    end
+end
 
 end
